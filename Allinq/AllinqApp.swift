cc10d54289bb55bffaa7c89a960da2f94eb278fd
Abdelmounaim <af180902@gmail.com>
2023-11-14 10:43:03 +0100

    Initial Commit

---
+++ Allinq/AllinqApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllinqApp.swift
+//  Allinq
+//
+//  Created by Abdelmounaim Fathi on 14/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct AllinqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
