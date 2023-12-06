36f0514ed7a04c63e75a0d49a4ed9257e51fb0b7
Abdelmounaim <af180902@gmail.com>
2023-12-06 19:08:35 +0100

    commit

--- Allinq/ARSessionManager.swift
+++ Allinq/ARSessionManager.swift
@@ -13,6 +13,7 @@ import Vision
 
 // MARK: - ARSession Manager
 
+// This class is responsible for starting and stopping the ARSession.
 class ARSessionManager: NSObject, ARSessionDelegate {
     static let shared = ARSessionManager()
     let log = OSLog(subsystem: Bundle.main.bundleIdentifier!, category: "ARSessionManager")
@@ -21,6 +22,9 @@ class ARSessionManager: NSObject, ARSessionDelegate {
     var visionRequests = [VNRequest]()
     var timer: Timer?
     let arView = ARView(frame: .zero)
+    var raycastResult: ARRaycastResult!
+    var detectedObjectPosition: SIMD3<Float>?
+    var addedBoxes = [AnchorEntity]()
 
     override private init() {
         super.init()
@@ -30,22 +34,29 @@ class ARSessionManager: NSObject, ARSessionDelegate {
 
     // MARK: - Start Session
 
+    // This method starts the ARSession and the classification request.
     func startSession(findObjectName: Binding<String?>, foundObject: Binding<Bool>) {
         stopSession()
-        session?.run(ARWorldTrackingConfiguration())
-        arView.session.run(ARWorldTrackingConfiguration())
+
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+
+        session?.run(configuration)
+        arView.session.run(configuration)
         startClassification(findObjectName: findObjectName, foundObject: foundObject)
         os_log("-- Session Started --")
     }
 
     // MARK: - Stop Session
 
+    // This method stops the ARSession and the classification request.
     func stopSession() {
         session?.pause()
         arView.session.pause()
         session = nil
         stopTimer()
         stopVision()
+        removeAddedBoxes()
         os_log("-- Session Stopped --")
     }
 
@@ -59,15 +70,55 @@ class ARSessionManager: NSObject, ARSessionDelegate {
         timer = nil
     }
 
+    private func removeAddedBoxes() {
+        for boxEntity in addedBoxes {
+            arView.scene.removeAnchor(boxEntity)
+        }
+        addedBoxes.removeAll()
+    }
+
+    // MARK: - Classification Timer
+
+    // This timer is responsible for starting the classification request every 2 seconds.
     private func startClassification(findObjectName: Binding<String?>, foundObject: Binding<Bool>) {
         arView.addCoaching()
-        timer = Timer.scheduledTimer(withTimeInterval: 3.0, repeats: true) { [self] _ in
+        timer = Timer.scheduledTimer(withTimeInterval: 2, repeats: true) { [self] _ in
             visionRequest(findObjectName: findObjectName, foundObject: foundObject)
         }
     }
 
+    private func addBox(at position: SIMD3<Float>) {
+        let sphereMesh = MeshResource.generateSphere(radius: 0.05)
+        let sphereEntity = ModelEntity(mesh: sphereMesh)
+
+        let material = SimpleMaterial(color: .systemBlue, isMetallic: false)
+        sphereEntity.model?.materials = [material]
+
+        let anchorEntity = AnchorEntity(world: position)
+        anchorEntity.addChild(sphereEntity)
+
+        arView.scene.addAnchor(anchorEntity)
+        addedBoxes.append(anchorEntity)
+    }
+
+    // MARK: - Raycasting
+
+    // This method performs a raycast from the center of the screen.
+    private func performRaycasting() -> ARRaycastResult? {
+        guard arView.session.currentFrame != nil else { return nil }
+
+        let results = arView.raycast(from: arView.center, allowing: .estimatedPlane, alignment: .any)
+
+        if let result = results.first {
+            return result
+        }
+
+        return nil
+    }
+
     // MARK: - Vision Classification Request
 
+    // This method creates a classification request using the Vision framework. It is responsible for detecting the object in the camera feed.
     private func visionRequest(findObjectName: Binding<String?>, foundObject: Binding<Bool>) {
         guard let currentFrame = arView.session.currentFrame else { return }
 
@@ -86,11 +137,20 @@ class ARSessionManager: NSObject, ARSessionDelegate {
 
             DispatchQueue.main.async {
                 if observation.identifier.lowercased() == findObjectName.wrappedValue?.lowercased() && !foundObject.wrappedValue {
-//                    self.stopSession()
                     self.stopTimer()
                     self.stopVision()
                     findObjectName.wrappedValue = observation.identifier
                     foundObject.wrappedValue = true
+
+                    if let raycastResult = self.performRaycasting() {
+                        self.detectedObjectPosition = SIMD3<Float>(raycastResult.worldTransform.columns.3.x,
+                                                                   raycastResult.worldTransform.columns.3.y,
+                                                                   raycastResult.worldTransform.columns.3.z)
+                    }
+
+                    if let position = self.detectedObjectPosition {
+                        self.addBox(at: position)
+                    }
 
                     #if DEBUG
                     os_log("findObjectName: %@, foundObject: %d, ###FOUND###: %@ (%.0f%)", log: self.log, type: .debug, findObjectName.wrappedValue ?? "nil", foundObject.wrappedValue, observation.identifier, observation.confidence * 100)
@@ -110,6 +170,9 @@ class ARSessionManager: NSObject, ARSessionDelegate {
     }
 }
 
+// MARK: - ARCoachingOverlayViewDelegate
+
+// This extension is responsible for adding the coaching overlay to the ARView.
 extension ARView: ARCoachingOverlayViewDelegate {
     func addCoaching() {
         let coachingOverlay = ARCoachingOverlayView()
