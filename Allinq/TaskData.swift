03d4ac28ce6c3f84f26af3f21392ab977d74ae7d
Abdelmounaim <af180902@gmail.com>
2023-12-12 22:33:40 +0100

    added documentation + improved vision request   to invoke with raycasting every 1 sec

---
+++ Allinq/TaskData.swift
@@ -0,0 +1,18 @@
+//
+//  TaskData.swift
+//  Allinq
+//
+//  Created by Abdelmounaim Fathi on 12/12/2023.
+//
+
+import SwiftUI
+
+struct TaskData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskData()
+}
